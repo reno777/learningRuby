4372cd797e404ead6749cef52729e777dbd56814
Reno <seth.rasmussen13@gmail.com>
2017-03-23 03:37:55 -0400

    added some of the basic prompts

--- simpleEncryption.rb
+++ simpleEncryption.rb
@@ -7,3 +7,26 @@ Author::    Seth Rasmussen (Reno)
 Copyright:: Copyright (c) 2016 Seth Rasmussen
 License::   MIT
 =end
+
+#variables
+
+
+
+
+#beginning
+puts "This is a simple encryption program using a Caesar cipher."
+puts " "
+puts "Please enter what you would like to be encrypted: "
+inputPhrase=gets.chomp
+inputPhrase.downcase!
+puts "Would you like to reverse your input before you encrypt? "
+choice=gets.chomp
+choice.downcase!
+
+if choice=="yes"
+  inputPhrase.reverse!
+end
+
+puts inputPhrase.capitalize
+
+#This is still in development
