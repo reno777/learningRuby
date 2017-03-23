3fdf4641b727220de9453dd2bc1a671c0fc1b8b6
Reno <seth.rasmussen13@gmail.com>
2017-03-23 03:59:27 -0400

    Added while loop for reversing prompt

--- simpleEncryption.rb
+++ simpleEncryption.rb
@@ -4,7 +4,7 @@
 This is just a small simple program that encrypts the users input. There will be added functionallity in the future to decrypt the input as well.
 
 Author::    Seth Rasmussen (Reno)
-Copyright:: Copyright (c) 2016 Seth Rasmussen
+Copyright:: Copyright (c) 2017 Seth Rasmussen
 License::   MIT
 =end
 
@@ -13,18 +13,30 @@ License::   MIT
 
 
 
-#beginning
+#beginning prompt
 puts "This is a simple encryption program using a Caesar cipher."
 puts " "
 puts "Please enter what you would like to be encrypted: "
 inputPhrase=gets.chomp
 inputPhrase.downcase!
-puts "Would you like to reverse your input before you encrypt? "
-choice=gets.chomp
-choice.downcase!
 
-if choice=="yes"
-  inputPhrase.reverse!
+
+#reverse input?
+while x=1
+
+  puts "Would you like to reverse your input before you encrypt? "
+  choice=gets.chomp
+  choice.downcase!
+
+  if choice=="yes"
+    inputPhrase.reverse!
+    break
+  elsif choice!="no"
+    puts"Please answer with yes or no."
+  else
+    break
+  end
+
 end
 
 puts inputPhrase.capitalize
