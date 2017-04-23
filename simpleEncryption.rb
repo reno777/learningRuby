59327c1d32702c9b3bdbb69f4c5106c5f99688c0
Reno <seth.rasmussen13@gmail.com>
2017-04-23 02:59:00 -0400

    Update simpleEncryption.rb

--- simpleEncryption.rb
+++ simpleEncryption.rb
@@ -4,6 +4,6 @@
 This is just a small simple program that encrypts the users input. There will be added functionallity in the future to decrypt the input as well.
 
 Author::    Seth Rasmussen (Reno)
-Copyright:: Copyright (c) 2016 Seth Rasmussen
+Copyright:: Copyright (c) 2017 Seth Rasmussen
 License::   MIT
 =end
